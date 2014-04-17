4861a867fd182ec0a75321d4ee6c5c0d6c34b194
Gibran Hemani <explodecomputer@gmail.com>
2014-04-17 12:22:36 +0100

    updated extractSnps.sh to allow specification of a keepfile

--- extractSnps/extractSnps.sh
+++ extractSnps/extractSnps.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# e.g. ./extractSnps.sh ../data/crapsnps.txt /ibscratch/wrayvisscher/imputation/twge/data/imputed/chrCHR/twge_1kg_p1v3_CHR ../data/twge_crap
+# e.g. ./extractSnps.sh snplist.txt 
 
 snplistfile=${1}
-plinkrt=${2}
-outfile=${3}
+keepfile=${2}
+plinkrt=${3}
+outfile=${4}
 
 touch ${outfile}_mergelist.txt
 rm ${outfile}_mergelist.txt
@@ -13,12 +14,14 @@ touch ${outfile}_mergelist.txt
 for i in {1..22}
 do
         filename=$(sed -e "s/CHR/$i/g" <<< ${plinkrt})
-        plink --noweb --bfile ${filename} --extract ${snplistfile} --make-bed --out ${outfile}_${i}
-        echo "${outfile}_${i}.bed ${outfile}_${i}.bim ${outfile}_${i}.fam" >> ${outfile}_mergelist.txt
+        plink1.90 --bfile ${filename} --keep ${keepfile} --extract ${snplistfile} --make-bed --out ${outfile}_${i}
+        if [ -f "${outfile}_${i}.bim" ]; then
+	        echo "${outfile}_${i}.bed ${outfile}_${i}.bim ${outfile}_${i}.fam" >> ${outfile}_mergelist.txt
+	   	fi
 done
 cat ${outfile}_mergelist.txt
 sed -i 1d ${outfile}_mergelist.txt
 cat ${outfile}_mergelist.txt
-plink --noweb --bfile ${outfile}_1 --merge-list ${outfile}_mergelist.txt --make-bed --out ${outfile}
+plink1.90 --bfile ${outfile}_1 --merge-list ${outfile}_mergelist.txt --make-bed --out ${outfile}
 
 rm ${outfile}_*
